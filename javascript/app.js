aa5e66ceb6014c6f1a174e1986c0345f61893d1e
Ian Ramos <ianramosc@gmail.com>
2017-03-21 17:10:29 -0300

    Added: Getting images from server

--- javascript/app.js
+++ javascript/app.js
@@ -16,7 +16,7 @@
      * @param {string} url
      * @param {function} onSuccess - success callback
      * @param {function} onError - error callback
-     * @memberof request
+     * @memberof Request
      */
     var get = function(url, onSuccess, onError) {
       var xhr = new XMLHttpRequest();
@@ -39,7 +39,7 @@
      * @param {object} data
      * @param {function} onSuccess - success callback
      * @param {function} onError - error callback
-     * @memberof request
+     * @memberof Request
      */
     var post = function(url, data, onSuccess, onError) {
       var xhr = new XMLHttpRequest();
@@ -70,7 +70,13 @@
   var Rendr = (function() {
     var state = {};
     var render = function() {};
-    
+
+    /**
+     * A function in Rendr (Rendr.setState).
+     * @function setState
+     * @param {obj} _state - an object describing what how state should change
+     * @memberof Rendr
+     */
     function setState(_state) {
       for(var key in _state) {
         state[key] = _state[key];
@@ -78,6 +84,12 @@
       render(state);
     }
 
+    /**
+     * A function in Rendr (Rendr.setRender).
+     * @function setRender
+     * @param {function} _renderFunc - a function that will be executed everytime state changes
+     * @memberof Rendr
+     */
     function setRender(_renderFunc) {
       render = _renderFunc;
     }
@@ -89,5 +101,37 @@
       setRender: setRender
     };
   })();
+
+  function createImagesList(imgList) {
+    var frag = document.createDocumentFragment();
+
+    imgList.forEach(function(url) {
+      frag.appendChild(createImagesListItem(url));
+    });
+
+    return frag;
+  }
+
+  function createImagesListItem(imgUrl) {
+    var li = document.createElement('li');
+    var img = document.createElement('img');
+
+    img.src = imgUrl;
+    img.className = "img-rounded";
+
+    li.appendChild(img);
+
+    return li;
+  }
+
+  Rendr.setRender(function(state) {
+    var list = document.getElementById('imagesList');
+    list.appendChild(createImagesList(state.imgList));
+  });
+  Rendr.setState({imgList: []});
+
+  Request.get(ROOT_URL + "/images", function(res) {
+    Rendr.setState({imgList: JSON.parse(res)});
+  });
 
 })();
